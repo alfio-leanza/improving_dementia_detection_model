86563c75df5f76b7dd7927d89d9716e6baf1cd9f
Alfio Leanza <alfioleanza8@icloud.com>
2025-05-09 10:02:38 +0200

    Forget to change the command python in python3

--- explainability-dementia-alfio/single_fold.sh
+++ explainability-dementia-alfio/single_fold.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
 # Dataset d1.0s o0.0s CWT
-python src/single_fold.py \
+python3 src/single_fold.py \
     --ds_name "miltiadous_deriv_uV_d1.0s_o0.0s" \
     --classes "hc-ftd-ad" \
-    --ds_parent_dir "/home/tom/fast2/gnn-datasets" \
+    --ds_parent_dir "/home/tom/dataset_eeg" \
     --device "cuda:0" \
     --seed 1234 \
     --batch_size 256 \
