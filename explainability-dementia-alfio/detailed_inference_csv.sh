2347c4a6f5715ed9b244b2f55c1d2aa27ce4f707
Alfio Leanza <alfioleanza8@icloud.com>
2025-05-12 10:27:22 +0200

    Ready to compute the inference

--- explainability-dementia-alfio/detailed_inference_csv.sh
+++ explainability-dementia-alfio/detailed_inference_csv.sh
@@ -1,8 +1,9 @@
 #!/bin/bash
 
-python src/detailed_inference_csv.py \
-    --timestamp "train_20250114_014153" \
-    --ds_parent_dir "/home/tom/fast2/gnn-datasets" \
+python3 src/detailed_inference_csv.py \
+    --timestamp "train_20250510_172519" \
+    --ds_parent_dir "/home/tom/dataset_eeg" \
     --ds_name "miltiadous_deriv_uV_d1.0s_o0.0s" \
     --classes "hc-ftd-ad" \
     --device "cuda:0" \
+    --output_dir "/home/alfio/improving_dementia_detection_model/explainability-dementia-alfio/local/detailed_inference/"
