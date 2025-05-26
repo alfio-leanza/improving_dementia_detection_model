d84e91085cc6315896929e4d1036d4bb9823e203
Alfio Leanza <alfioleanza8@icloud.com>
2025-05-26 16:09:28 +0200

    Debug

--- explainability-dementia-alfio/src/run_monitor_gnn.sh
+++ explainability-dementia-alfio/src/run_monitor_gnn.sh
@@ -5,7 +5,7 @@ CSV_TP="/home/alfio/improving_dementia_detection_model/explainability-dementia-a
 CKPT_GNN="/home/alfio/improving_dementia_detection_model/explainability-dementia-alfio/local/checkpoints/train_20250510_172519/best_test_acc.pt"
 CWT_DIR="/home/tom/dataset_eeg/miltiadous_deriv_uV_d1.0s_o0.0s/cwt"
 
-python train_monitor_gnn_fromcsv.py \
+python3 monitor_gnn.py \
   --csv_true_pred "${CSV_TP}" \
   --ckpt_gnn "${CKPT_GNN}" \
   --cwt_dir "${CWT_DIR}" \
