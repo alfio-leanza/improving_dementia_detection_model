294021b5aeba99409338cc2ce8308b9276b0360a
Alfio Leanza <alfioleanza8@icloud.com>
2025-06-26 17:31:09 +0200

    Debug

--- explainability-dementia-alfio/src/embeddings/run.sh
+++ explainability-dementia-alfio/src/embeddings/run.sh
@@ -2,7 +2,7 @@
 python3 extract_embeddings.py \
   --annot_csv  /home/tom/dataset_eeg/miltiadous_deriv_uV_d1.0s_o0.0s/annot_all_hc-ftd-ad.csv \
   --crops_dir  /home/tom/dataset_eeg/miltiadous_deriv_uV_d1.0s_o0.0s/cwt \
-  --checkpoint /home/alfio/improving_dementia_detection_model/explainability-dementia-alfio/local/embeddings/checkpoint/checkpoint_mk11.pt \
+  --checkpoint /home/alfio/improving_dementia_detection_model/explainability-dementia-alfio/local/checkpoints/train_20250510_172519/best_test_acc.pt \
   --output /home/alfio/improving_dementia_detection_model/explainability-dementia-alfio/local/embeddings/checkpoint/embeddings.parquet
 
 # 2) Pooling per paziente
