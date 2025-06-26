633840774f947a0955c48356c611e5866fcd6414
Alfio Leanza <alfioleanza8@icloud.com>
2025-06-26 18:26:50 +0200

    Aggregate embeddings per patience

--- explainability-dementia-alfio/src/embeddings/run.sh
+++ explainability-dementia-alfio/src/embeddings/run.sh
@@ -1,9 +1,11 @@
 # 1) Estrazione segmenti → Parquet
-python3 extract_embeddings.py \
-  --annot_csv  /home/tom/dataset_eeg/miltiadous_deriv_uV_d1.0s_o0.0s/annot_all_hc-ftd-ad.csv \
-  --crops_dir  /home/tom/dataset_eeg/miltiadous_deriv_uV_d1.0s_o0.0s/cwt \
-  --checkpoint /home/alfio/improving_dementia_detection_model/explainability-dementia-alfio/local/checkpoints/train_20250510_172519/best_test_acc.pt \
-  --output /home/alfio/improving_dementia_detection_model/explainability-dementia-alfio/local/embeddings/checkpoint/embeddings.parquet
+#python3 extract_embeddings.py \
+ # --annot_csv  /home/tom/dataset_eeg/miltiadous_deriv_uV_d1.0s_o0.0s/annot_all_hc-ftd-ad.csv \
+  #--crops_dir  /home/tom/dataset_eeg/miltiadous_deriv_uV_d1.0s_o0.0s/cwt \
+  #--checkpoint /home/alfio/improving_dementia_detection_model/explainability-dementia-alfio/local/checkpoints/train_20250510_172519/best_test_acc.pt \
+  #--output /home/alfio/improving_dementia_detection_model/explainability-dementia-alfio/local/embeddings/checkpoint/embeddings.parquet
 
 # 2) Pooling per paziente
-#python aggregate_patient_embeds.py --input embeddings.parquet
+python aggregate_patient_embeds.py \
+ --input /Volumes/Extreme SSD/progetto_tesi/improving_dementia_detection_model/explainability-dementia-alfio/local/embeddings/checkpoint/embeddings.parquet \
+ --output /home/alfio/improving_dementia_detection_model/explainability-dementia-alfio/local/embeddings/checkpoint/patient_embeds.pkl
